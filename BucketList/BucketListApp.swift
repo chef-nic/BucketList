5b0c7ab1b55a2f04144b6de964bdfc2f23ce4ed3
chef-nic <nicholasajohnson@me.com>
2024-07-30 14:08:57 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Nicholas Johnson on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
