c0f0eafa3ae36cee633137994c2dc905e07ffa00
chef-nic <nicholasajohnson@me.com>
2024-07-31 10:56:08 -0700

    added custom location markers and #if DEBUG and #endif to prevent example data from showing up in app store release

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,17 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var locations = [Location]()
+    
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34, longitude: -118.5),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
+    @State private var isUnlocked = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                    }
+                }
+            }
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
         }
-        .padding()
     }
+    
+    
 }
 
 #Preview {
